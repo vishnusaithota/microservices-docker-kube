cc8ed93e25812cdb122e037194faa84c07fadf1f
tnvrravish <vt7@njit.edu>
2023-06-21 23:56:04 -0400

    required dependencies added for fraud and customer and register with eureka server

--- fraud/src/main/java/com/spiderweb/fraud/FraudApplication.java
+++ fraud/src/main/java/com/spiderweb/fraud/FraudApplication.java
@@ -5,11 +5,13 @@ import com.spiderweb.fraud.repository.FraudRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import java.time.LocalDateTime;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FraudApplication {
 
     public static void main(String[] args) {
