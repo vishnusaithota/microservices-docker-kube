fa1c076a7eb9bbee75ec1f699c8b4607c650565c
tnvrravish <vt7@njit.edu>
2023-06-22 23:07:32 -0400

    configured open-feign and created a FraudClient Interface

---
+++ clients/src/main/java/com/spiderweb/clients/fraud/FraudCheckResponse.java
@@ -0,0 +1,6 @@
+package com.spiderweb.clients.fraud;
+
+public record FraudCheckResponse(
+        Boolean isFraudster
+) {
+}
---
+++ clients/src/main/java/com/spiderweb/clients/fraud/FraudClient.java
@@ -0,0 +1,13 @@
+package com.spiderweb.clients.fraud;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient("fraid")
+public interface FraudClient {
+
+    @GetMapping("api/v1/fraud{customerId}")
+    FraudCheckResponse checkFraudster(
+            @PathVariable("customerId") Integer customerId);
+}
