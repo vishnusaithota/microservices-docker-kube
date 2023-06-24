df57802757b5e8bbc8aad4c8b158adf9cf680b00
tnvrravish <vt7@njit.edu>
2023-06-24 17:53:24 -0400

    set up RabbitMQ and configured it

--- notification/src/main/java/com/spiderweb/notification/NotificationApplication.java
+++ notification/src/main/java/com/spiderweb/notification/NotificationApplication.java
@@ -1,11 +1,20 @@
 package com.spiderweb.notification;
 
+import com.spiderweb.amqp.RabbitMQMessageProducer;
+import com.spiderweb.notification.config.NotificationConfig;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.spiderweb.notification",
+                "com.spiderweb.amqp"
+        }
+)
 @EnableEurekaClient
 @EnableFeignClients(
         basePackages = "com.spiderweb.clients"
@@ -14,4 +23,20 @@ public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class,args);
     }
+
+//    @Bean
+//    CommandLineRunner commandLineRunner(
+//            RabbitMQMessageProducer producer,
+//            NotificationConfig notificationConfig) {
+//        return args -> {
+//            producer.publish(
+//                    new Person("Vishnu", 19),
+//                    notificationConfig.getInternalExchange(),
+//                    notificationConfig.getInternalNotificationRoutingKey());
+//        };
+//    }
+//
+//    record Person(String name, int age){}
 }
+
+
--- notification/src/main/java/com/spiderweb/notification/controller/NotificationController.java
+++ notification/src/main/java/com/spiderweb/notification/controller/NotificationController.java
@@ -4,6 +4,7 @@ import com.spiderweb.clients.notification.NotificationRequest;
 import com.spiderweb.notification.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
