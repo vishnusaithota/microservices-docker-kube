d8a203a769f91648a5f9712145ea6ca0788472bd
tnvrravish <vt7@njit.edu>
2023-07-04 12:38:35 -0400

    Kafka set up completed

--- kafka/src/main/java/com/spiderweb/kafka/model/Message.java
+++ kafka/src/main/java/com/spiderweb/kafka/model/Message.java
@@ -1,9 +1,10 @@
 package com.spiderweb.kafka.model;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 public record Message(
         String message,
         LocalDateTime createdAt
-) {
+) implements Serializable {
 }
